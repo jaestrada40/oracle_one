cb45706e4b29b786ece4fbdc613e6aa28920376e
jaestrada40 <javiera.estradag@gmail.com>
2022-08-06 20:03:35 -0600

    Update script.js

--- Alura Challenge/js/script.js
+++ Alura Challenge/js/script.js
@@ -2,3 +2,10 @@ function init()
 {
     document.getElementById("exampleFormControlTextarea1").focus();
 }
+
+
+
+function myFunction() {
+    document.getElementById("myP").style.visibility = "hidden";
+    
+  }
